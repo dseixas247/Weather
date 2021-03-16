d0fca49e39f99f7fe8e4faed9b1cb5886d2f7d9a
dseixas247 <80399138+dseixas247@users.noreply.github.com>
2021-03-16 18:40:40 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var unirest = require('unirest');
 
 http.createServer(function (req, res) {
 
-  var q = url.parse(req.url, true).query;
-  var cityname = q.cityname;
+    var q = url.parse(req.url, true).query;
+    var cityname = q.cityname;
 
-  fs.readFile('Node/Weather/form.html', function(err, data) {
-
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-
-    if (cityname!=null) {
-        return res.end(cityname)
+    function checkweather(city){
+        unirest
+        .post('https://api.openweathermap.org/data/2.5/weather?appid=2bc0eb60cbe0d0307856124129e7a460&units=metric&q=' + city)
+        .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+        .end((response) => {
+            if(response.body.cod == 404){
+                console.log("Cidade não foi encontrada");
+            }
+            else{
+                console.log(city + ": Estão " + response.body.main.temp + " graus Celsius");
+            }
+        });
     }
 
-    else {
-        return res.end();
-    }
+    fs.readFile('Node/Weather/form.html', function(err, data) {
 
-  });
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } 
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+
+        if(cityname!=null){
+            checkweather(cityname);
+        }
+
+        res.end();
+
+    });
 
 }).listen(3000);
 
